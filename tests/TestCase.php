2ee147787cb244c9dd7a990431e89e378032e61a
Axel Pardemann <axel.pardemann@norse.blue>
2019-07-06 10:16:35 -0500

    Init

---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NorseBlue\StringExtensions\Inflections\Tests;
+
+use PHPUnit\Framework\TestCase as TestCaseBase;
+
+class TestCase extends TestCaseBase
+{
+}
